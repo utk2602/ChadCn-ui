e0303e1cd960d277e185e69a88db00fa60a4c31c
utkarsh <166751207+utk2602@users.noreply.github.com>
2025-08-19 18:00:22 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -215,7 +215,7 @@ export const hyperspeedPresets = {
 
 export default function Home() {
   return (
-    <div className="w-full h-full">
+    <div className="w-screen h-full">
 
       <Hyperspeed effectOptions={hyperspeedPresets.one} />
       
