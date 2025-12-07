7e789b11823556d172946432c5b1da81e30f5fbf
utkarsh <166751207+utk2602@users.noreply.github.com>
2025-12-07 18:25:41 +0530

    style: fix formatting and spacing in HomePage component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import { About } from '@/components/sections/about';
 import { Components } from '@/components/sections/components';
 import { Contact } from '@/components/sections/contact';
- import { Features } from '@/components/sections/features';
+import { Features } from '@/components/sections/features';
 import { Footer } from '@/components/sections/footer';
 import { Hero } from '@/components/sections/hero';
 import { HomeBanner } from '../../components/sections/home-banner';
@@ -10,12 +10,12 @@ import { HomeNav } from '@/components/sections/home-nav';
 export default function HomePage() {
   return (
     <>
-       <HomeBanner /> 
+      <HomeBanner />
       <HomeNav />
       <Hero />
       <About />
       <Components />
-      <Features /> 
+      <Features />
       <Contact />
       <Footer />
     </>
