cec0855f1ff2f30540ec4ee507d3e89654e77c6c
utkarsh <166751207+utk2602@users.noreply.github.com>
2025-12-07 17:17:17 +0530

    feat: restore HomeBanner and Features components in HomePage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,21 +1,21 @@
 import { About } from '@/components/sections/about';
 import { Components } from '@/components/sections/components';
 import { Contact } from '@/components/sections/contact';
-// import { Features } from '@/components/sections/features';
+ import { Features } from '@/components/sections/features';
 import { Footer } from '@/components/sections/footer';
 import { Hero } from '@/components/sections/hero';
-// import { HomeBanner } from '@/components/sections/home-banner';
+import { HomeBanner } from '@/components/sections/home-banner';
 import { HomeNav } from '@/components/sections/home-nav';
 
 export default function HomePage() {
   return (
     <>
-      {/* <HomeBanner /> */}
+       <HomeBanner /> 
       <HomeNav />
       <Hero />
       <About />
       <Components />
-      {/* <Features /> */}
+      <Features /> 
       <Contact />
       <Footer />
     </>
